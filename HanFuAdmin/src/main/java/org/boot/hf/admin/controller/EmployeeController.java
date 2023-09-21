e4c8522c6ac62d4c52e5a5a3f2fc5cd89a5d4da6
TxsLs <l1597757959@163.com>
2023-09-21 16:52:11 +0800

    测试no..........aaaaa

--- HanFuAdmin/src/main/java/org/boot/hf/admin/controller/EmployeeController.java
+++ HanFuAdmin/src/main/java/org/boot/hf/admin/controller/EmployeeController.java
@@ -43,7 +43,7 @@ import springfox.documentation.annotations.ApiIgnore;
 @RequestMapping("/employee")
 public class EmployeeController extends BaseController<Employee, EmployeeService> {
 
-	@ApiOperation(value = "添加员工实体", notes = "允许文件上传")
+	@ApiOperation(value = "添加员工实体", notes = "允许文件上传！")
 	@ApiImplicitParams({ @ApiImplicitParam(name = "code", value = "代码", required = true, paramType = "form"),
 			@ApiImplicitParam(name = "name", value = "名称", required = true, paramType = "form"),
 			@ApiImplicitParam(name = "gender", value = "性别(1-男,2-女)", required = true, paramType = "form", dataType = "int"),
