b8ebc32e554c8c0dea182962476431d7603cd19d
TxsLs <l1597757959@163.com>
2023-10-10 19:40:06 +0800

    修改了请求地址--10.10pm--txs

--- HanFu_Admin_Front/dist/js/common.js
+++ HanFu_Admin_Front/dist/js/common.js
@@ -11,7 +11,7 @@ if (typeof rock === "undefined") {
  * 在这里修改提供mvc rest接口的服务器url地址
  */
 //var mvc_base_url = "/hanfu/";
-var mvc_base_url = "http://localhost:8080/hanfu/";
+var mvc_base_url = "http://127.0.0.1:8080/hanfu/";
 
 /**
  * 调用该方法初始化业务服务对象
