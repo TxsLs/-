7442794d1cd7065b26d07239a20f53afac438f04
wxj2333 <2431133982@qq.com>
2023-10-17 09:57:12 +0800

    完成店铺信息修改，主页也可以显示用户名与头像--10.17

---
+++ hanfu_SJfront/pagejs/index.js
@@ -0,0 +1,91 @@
+$(document).ready(function () {
+
+    // var {
+    //   OverlayScrollbars,
+    //   ScrollbarsHidingPlugin,
+    //   SizeObserverPlugin,
+    //   ClickScrollPlugin
+    // } = OverlayScrollbarsGlobal;
+    // const osInstance = OverlayScrollbars(document.querySelector('body'), {});
+
+
+    //头部搜索框处理(不需要可以删除,不明白的可以看bootstrap-admin官方文档)
+    $(document).on('search.bsa.navbar-search', function (e, inputValue, data) {
+      //先得到请求地址,组合后大概是这样pages/search.html?keyword=dsadsa&type=article&user=admin2
+      let url = data.action + '?keyword=' + inputValue + '&' + $.param(data.params);
+
+      //然后通过tab打开一个搜索结果的窗口
+      Quicktab.get('.qtab').addTab({
+        title: '<i class="bi bi-search"></i><span class="text-danger ms-2">' + inputValue + '</span>',
+        url: url,
+        close: true,
+      })
+    })
+
+
+    $(function ($) {
+
+_root.loginUser({}, function (rtn) {
+    var ssoUser=null;
+    if (rtn.hasError) {
+      alert(rock.errorText(rtn, "获得当前用户失败！"));
+    } else {
+      ssoUser = rtn.result;
+    }
+    if (rock.isNull(ssoUser)) {
+      $("#loginUser").html("未知用户请登录！");
+      document.getElementById("login-dropdown").style.display = "block";
+      document.getElementById("logout-dropdown").style.display = "none";
+    } else {
+      $("#loginUser").html(ssoUser.name);
+      document.getElementById("login-dropdown").style.display = "none";
+      document.getElementById("logout-dropdown").style.display = "block";
+
+        //登录后显示头像
+        var mvc = rock.initSvr(["merchant"]);
+        var Service = mvc.findService("merchant");
+        if (ssoUser.hasPhoto) {
+
+          $("#userImage").attr("src", Service.url("photo") + "?id=" + ssoUser.id);
+        }
+    }
+  })
+});
+
+ //退出登录
+ $(document).on('click', '.bsa-logout', function (e) {
+    e.preventDefault();
+    $.modal({
+      body: '确定要退出吗？',
+      cancelBtn: true,
+      ok: function () {
+
+        _root.logout({}, (rtn) => {
+          if (rtn.hasError || !rtn.result) {
+            $.toasts({
+              type: 'danger',
+              content: '注销登录出错!',
+
+            })
+
+          } else {
+
+            $.toasts({
+              type: 'success',
+              content: '退出登录成功！',
+              onHidden: function () {
+                top.location.replace('pages/login.html');
+              }
+            })
+
+          }
+        });
+
+
+
+      }
+    })
+
+
+  });
+  })
