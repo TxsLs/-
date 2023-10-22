a93c059d1eea90f5be66b3d0fdae51fccca90489
TxsLs <l1597757959@163.com>
2023-10-22 21:40:33 +0800

    找回密码页和提交解封申请，下一步在管理员页面中显示提交的申请--10.22pm--txs

---
+++ HanFu_Admin_Front/pagejs/unlock.js
@@ -0,0 +1,113 @@
+function redirectToLogin() {
+    window.location.href = "login.html";
+  }
+
+$(function () {
+    //前端表单验证
+
+    $('#form').formValidation({
+
+        fields: {
+            code: {
+                validators: {
+                    notEmpty: {
+                        message: '账号不能为空'
+                    },
+                    stringLength: {
+                        min: 1,
+                        max: 20,
+                        message: '用户名长度必须在1到20个字符之间'
+                    }
+                }
+            },
+            name: {
+                validators: {
+                    notEmpty: {
+                        message: '姓名不能为空'
+                    },
+                    stringLength: {
+                        min: 1,
+                        max: 20,
+                        message: '用户名长度必须在1到20个字符之间'
+                    }
+                }
+            },
+            phone: {
+                validators: {
+                    notEmpty: {
+                        message: '电话号码不能为空'
+                    },
+                    regexp: {
+                        regexp: /^\d{11}/,
+                        message: '请输入有效的11位电话号码!'
+                    }
+                }
+            },
+            reason: {
+                validators: {
+                    notEmpty: {
+                        message: '理由不能为空！'
+                    },
+                    stringLength: {
+                        min: 1,
+                        max: 200,
+                        message: '封禁理由长度必须在1到200个字之间'
+                    }
+                }
+            }
+
+
+
+        }
+    }).on('success.form.fv', function (e) {
+
+        //阻止表单提交
+        e.preventDefault();
+
+        var reason = $('#reason').val();
+        var code = $('#code').val();
+        console.log(code)
+        var name = $('#name').val();
+        var phone = $('#phone').val();
+        var dataToSend = {
+            reason: reason,
+            code: code,
+            type: 1,
+            status: 0,
+            name: name,
+            phone: phone
+        };
+
+        //发起ajax请求
+        $.ajax({
+            method: 'Post',
+            url: 'http://127.0.0.1:8080/hanfu/unlock/addRequest',
+            //表单数据
+            contentType: 'application/json',
+
+            data: JSON.stringify(dataToSend),
+        }).then(response => {
+            if (response.result) {
+                $.toasts({
+                    type: 'success',
+                    content: '申请提交成功！你的申请将在七个工作日类受理，请耐心等待！',
+                    delay: 3500,
+                    onHidden: function () {
+                        location.href = 'login.html';
+                    }
+                });
+            } else if (response.hasError) {
+                var errorMessage = rock.errorText(response, "提交申请失败!");
+                $.toasts({
+                    type: 'danger',
+                    content: errorMessage,
+                    delay: 3100, // 将显示时间设置
+                });
+
+
+            }
+  
+
+        });
+    });
+});
