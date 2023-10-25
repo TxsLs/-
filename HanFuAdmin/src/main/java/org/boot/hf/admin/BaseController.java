0eb0be4ea75da1c418da3fce126b136ca6e82dba
TxsLs <l1597757959@163.com>
2023-10-25 22:28:12 +0800

    修改basecontroller中删除多个实体传入的参数--10.25pm--txs

--- HanFuAdmin/src/main/java/org/boot/hf/admin/BaseController.java
+++ HanFuAdmin/src/main/java/org/boot/hf/admin/BaseController.java
@@ -110,7 +110,7 @@ public abstract class BaseController<T extends Entity, S extends Service<T>> {
 	@ApiOperation(value = "删除多个实体", notes = "该接口继承自BaseController")
 	@ApiImplicitParam(name = "id", value = "多个主键id", required = true, dataType = "long", allowMultiple = true)
 	@RequestMapping(value = "/removeMore", method = { RequestMethod.GET })
-	public @ResponseBody Result<Boolean> removeMore(@NotEmpty @RequestParam("id") Long[] ids) {
+	public @ResponseBody Result<Boolean> removeMore(@NotEmpty @RequestParam("id[]") Long[] ids) {
 		log.debug("call removeMore");
 		boolean result = this.service().deleteMore(Arrays.asList(ids));
 		return Result.of(result);
