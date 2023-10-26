6da152397f98ebfd33b3ad49a965eb31d8a53dbd
TxsLs <l1597757959@163.com>
2023-10-26 11:48:37 +0800

    商家订单查询后台基本完成--10.26am--txs

--- hanfushopping/src/main/java/com/example/demo/controller/OrderDetailController.java
+++ hanfushopping/src/main/java/com/example/demo/controller/OrderDetailController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.apache.commons.lang3.StringUtils;
 import org.quincy.rock.core.dao.DaoUtil;
 import org.quincy.rock.core.dao.sql.Predicate;
@@ -28,33 +30,62 @@ import lombok.extern.slf4j.Slf4j;
 @Api(tags = "订单详情管理模块")
 @Controller
 @RequestMapping("/orderdetail")
-public class OrderDetailController extends BaseController <OrderDetail, OrderDetailService>{
-
+public class OrderDetailController extends BaseController<OrderDetail, OrderDetailService> {
 
 	@ApiOperation(value = "条件分页查询", notes = "")
-	@ApiImplicitParams({ 
-		@ApiImplicitParam(name = "orderId", value = "订单id", dataType = "long"),
-		@ApiImplicitParam(name = "searchCode", value = "代码(支持like)，允许null"),
+	@ApiImplicitParams({ @ApiImplicitParam(name = "orderId", value = "订单id", dataType = "long"),
+			@ApiImplicitParam(name = "searchCode", value = "代码(支持like)，允许null"),
 			@ApiImplicitParam(name = "searchName", value = "名称(支持like)，允许null"),
-			@ApiImplicitParam(name = "sort", value = "排序规则字符串"),
+			@ApiImplicitParam(name = "merchantId", value = "商家id"), @ApiImplicitParam(name = "sort", value = "排序规则字符串"),
 			@ApiImplicitParam(name = "pageNum", value = "页码", required = true, dataType = "long"),
 			@ApiImplicitParam(name = "pageSize", value = "页大小", required = true, dataType = "int") })
 	@RequestMapping(value = "/queryPage", method = { RequestMethod.GET })
-	public @ResponseBody Result<PageSet<? extends Entity>> queryPage(Long orderId,String searchCode, String searchName, String sort,
+	public @ResponseBody Result<PageSet<? extends Entity>> queryPage(Long merchantId, String productName,
+			String totalPrice, String orderStatus, Long orderId, String searchCode, String searchName, String sort,
 			@RequestParam("pageNum") long pageNum, @RequestParam int pageSize) {
 		log.debug("call queryPage");
 		Predicate where = DaoUtil.and();
 		if (orderId != null)
 			where.equal(DataType.LONG, "orderId", orderId.toString());
-		if (StringUtils.isNotEmpty(searchCode)) {
-			where.like("code", searchCode);
+		if (StringUtils.isNotEmpty(productName)) {
+			where.like("productName", productName);
 		}
-		if (StringUtils.isNotEmpty(searchName)) {
-			where.like("name", searchName);
+		if (StringUtils.isNotEmpty(totalPrice)) {
+			where.like("totalPrice", totalPrice);
 		}
+		if (StringUtils.isNotEmpty(orderStatus)) {
+			where.like("orderStatus", orderStatus);
+		}
+
+		if (merchantId != null)
+			where.equal(DataType.LONG, "merchantId", merchantId.toString());
 		PageSet<OrderDetail> ps = this.service().findPage(where, Sort.parse(sort), pageNum, pageSize);
 		return Result.toResult(ps);
 	}
-	
-	
+
+	@ApiOperation(value = "根据指定的属性名和值删除所有数据", notes = "该接口继承自SimpleController")
+	@RequestMapping(value = "/delAllByName", method = { RequestMethod.GET })
+	public @ResponseBody Result<Boolean> delAllByName(Long orderId, String sort) {
+		log.debug("call delAllByName");
+		Predicate where = DaoUtil.and();
+		if (orderId != null)
+			where.equal(DataType.LONG, "orderId", orderId.toString());
+		boolean vo = this.service().deleteAll(where);
+		return Result.toResult(vo);
+
+	}
+
+	@ApiOperation(value = "根据指定的属性名和值返回所有数据", notes = "该接口继承自SimpleController")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "propName", value = "属性名", required = true),
+			@ApiImplicitParam(name = "propValue", value = "属性值", required = true) })
+	@RequestMapping(value = "/queryAllByName", method = { RequestMethod.GET })
+	public @ResponseBody Result<List<OrderDetail>> queryAllByName(@RequestParam String propName,
+			@RequestParam Object propValue, String sort) {
+		log.debug("call queryAllByName");
+
+		List<OrderDetail> vo = this.service().findAllByName(propName, propValue, Sort.parse(sort));
+		return Result.toResult(vo);
+
+	}
+
 }
--- hanfushopping/src/main/java/com/example/demo/entity/OrderDetail.java
+++ hanfushopping/src/main/java/com/example/demo/entity/OrderDetail.java
@@ -1,5 +1,8 @@
 package com.example.demo.entity;
 
+import org.quincy.rock.core.dao.annotation.Column;
+import org.quincy.rock.core.dao.annotation.IgnoreInsert;
+import org.quincy.rock.core.dao.annotation.IgnoreUpdate;
 import org.quincy.rock.core.dao.annotation.JoinTable;
 import org.quincy.rock.core.dao.annotation.JoinTables;
 import org.quincy.rock.core.dao.annotation.Table;
@@ -13,17 +16,59 @@ import lombok.Setter;
 @Setter
 @ApiModel(description = "商品详情实体(在执行更新操作时采取动态更新策略，如果属性值为空，则忽略该属性)")
 @Table(name = "t_order_detail", alias = "od")
-@JoinTables({ @JoinTable(name = "t_order_table", alias = "o", onExpr = "o.f_id=od.f_order_id")})
-public class OrderDetail extends Entity{
+@JoinTables({ @JoinTable(name = "t_order_table", alias = "o", onExpr = "o.f_id=od.f_order_id"),
+		@JoinTable(name = "t_product", alias = "p", onExpr = "od.f_product_id=p.f_id") })
+public class OrderDetail extends Entity {
 	private static final long serialVersionUID = 6832315456734074806L;
 
-
 	@ApiModelProperty(value = "订单id", required = true, position = 1)
-    private String orderId;
+	private String orderId;
 
-    @ApiModelProperty(value = "商品id", position = 2)
-    private String productId;
+	@ApiModelProperty(value = "商品id", position = 2)
+	private String productId;
 
-    @ApiModelProperty(value = "商品数量", position = 3)
-    private String quantity;
+	@ApiModelProperty(value = "商品数量", position = 3)
+	private String quantity;
+
+	@ApiModelProperty(value = "商品名称", position = 4)
+	@Column(value = "f_name", tableAlias = "p", ignoreInsert = true, ignoreUpdate = true)
+	private String productName;
+
+	@ApiModelProperty(value = "商品价格", position = 5)
+	@Column(value = "f_price", tableAlias = "p", ignoreInsert = true, ignoreUpdate = true)
+	private String productPrice;
+
+	@ApiModelProperty(value = "商家id", position = 6)
+	@Column(value = "f_merchant_id", tableAlias = "p", ignoreInsert = true, ignoreUpdate = true)
+	private String merchantId;
+	
+	@ApiModelProperty(value = "顾客地址", position = 7)
+	@Column(value = "f_address", tableAlias = "o", ignoreInsert = true, ignoreUpdate = true)
+	private String address;
+	
+	@ApiModelProperty(value = "订单code", position = 8)
+	@Column(value = "f_code", tableAlias = "o", ignoreInsert = true, ignoreUpdate = true)
+	private String code;
+	
+	@ApiModelProperty(value = "订单总价", position = 9)
+	@Column(value = "f_total_price", tableAlias = "o", ignoreInsert = true, ignoreUpdate = true)
+	private String totalPrice;
+	
+	@ApiModelProperty(value = "订单状态", position = 10)
+	@Column(value = "f_order_status", tableAlias = "o", ignoreInsert = true, ignoreUpdate = true)
+	private String orderStatus;
+	
+	
+	@ApiModelProperty(value = "创建时间", position = 11)
+	@Column(value = "f_created_time", tableAlias = "o", ignoreInsert = true, ignoreUpdate = true)
+	private String createdTime;
+
+	@ApiModelProperty(value = "更新时间", position = 12)
+	@Column(value = "f_updated_time", tableAlias = "o", ignoreInsert = true, ignoreUpdate = true)
+	private String updatedTime;
+	
+	@ApiModelProperty(value = "手机号", position = 13)
+	@Column(value = "f_phone", tableAlias = "o", ignoreInsert = true, ignoreUpdate = true)
+	private Long phone;
+	
 }
